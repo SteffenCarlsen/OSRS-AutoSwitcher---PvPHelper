17978fcba7be649b6fc486e7c15e26bfa89b08cd
SDCAAU <bstaffe@gmail.com>
2018-08-27 20:36:16 +0200

    Improved application logic + Added AutoEater + Added AutoSpecial

--- AutoSwitcher/Model/AutoSwitchData.cs
+++ AutoSwitcher/Model/AutoSwitchData.cs
@@ -10,14 +10,23 @@ namespace AutoSwitcher.Model
 {
     public class AutoSwitchData
     {
-        public List<InventoryItem> InventoryData { get; set; }
+        public List<InventoryItem> InventoryDataAutoSwitch { get; set; }
         public char? KeyboardHotkey { get; set; }
-        public MouseButtons? MouseHotkey { get; set; }
+        public char? FoodHotkey { get; set; }
+        public char? CombofoodHotkey { get; set; }
+        public char? PrayerHotkey { get; set;}
+        public char? BrewHotkey { get; set; }
+        public char? SpecChar { get; set; }
         public bool ReturnOldPos { get; set; }
         public bool Stretched { get; set; }
-        public int speed { get; set; }
+        public int Speed { get; set; }
         public Point FirstInventSlot { get; set; }
+        public Point FirstPrayerSlot { get; set; }
+        public Point SpecBarPoint { get; set; }
         public string InventKey { get; set; }
+        public PrayerBook.Prayer SpecOnePrayer { get; set; }
+        public PrayerBook.Prayer SpecTwoPrayer { get; set; }
+
 
     }
 }
